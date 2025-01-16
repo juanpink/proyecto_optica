03b85ec89d751ae6ef44cba804e23cd27e37b743
juanpink <jmiopin6@gmail.com>
2025-01-16 13:44:46 +0100

    añadiendo routerLink para navegar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NuevoClienteComponent } from './components/nuevo-cliente/nuevo-cliente.component';
+import { ConsultaClienteComponent } from './components/consulta-cliente/consulta-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ConsultaClienteComponent, pathMatch: 'full'},
+    {path: 'nuevo-cliente', component: NuevoClienteComponent, pathMatch: 'full'},
+    {path: 'consulta-cliente', component: ConsultaClienteComponent, pathMatch: 'full'},
+    {path: '**', component: ConsultaClienteComponent, pathMatch: 'full'},
+];
